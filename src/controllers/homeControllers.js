e34a3032880dc589f9931507cee83286283d41bb
hongnm2202 <94152885+hongnm2202@users.noreply.github.com>
2023-02-08 22:14:10 +0700

    push code last day 2-8-2023

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,6 +1,15 @@
+const connection = require("../config/database")
+
 const getHome = (req, res) => {
-    res.send("<h1>Hello World! with NMH with NODEMON</h1>")
+    //process data
+    let user = []
+    connection.query("SELECT * FROM Users", function (err, results, fields) {
+        user = results
+        console.log(results) // results contains rows returned by server
+        res.send(JSON.stringify(user))
+    })
 }
+
 const getStaticFile = (req, res) => {
     // res.send("<h1>Hello Manhhong!</h1>")
     res.render("sample.ejs")
--- src/sever.js
+++ src/sever.js
@@ -3,7 +3,6 @@ const path = require("path")
 require("dotenv").config()
 const configViewEngine = require("./config/viewEngine")
 const Routers = require("./routes/web")
-const connection = require("./config/database")
 const app = express()
 const port = process.env.PORT || 8888
 const hostname = process.env.HOST_NAME
@@ -13,13 +12,6 @@ configViewEngine(app)
 
 // khai báo routes
 app.use("/", Routers)
-
-// test connection
-
-// simple query
-connection.query("SELECT * FROM Users", function (err, results, fields) {
-    console.log(results) // results contains rows returned by server
-})
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
